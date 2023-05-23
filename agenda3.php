ffc8bccb91b5243928ed23b1bf3c476f4caed7a3
Almelo134 <alissonhsmelo@gmail.com>
2023-05-22 22:20:26 -0300

    Update SENAI

--- agenda3.php
+++ agenda3.php
@@ -6,10 +6,9 @@
     header("Location: index.php");
     exit();
 }
-  
+
   require 'PHP/conexao/banco.php';
 
-  $dataEntrega = date('d/m/Y');
 
 ?>
 
@@ -18,23 +17,31 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Admin</title>
     <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="assets/css/calendario.css">
+    <title>Admin</title> 
+    
+
+
+    <!-- <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css"> -->
+    <!-- <link rel="stylesheet" href="assets/vendors/flag-icon-css/css/flag-icon.min.css"> -->
+
+  
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="shortcut icon" href="assets/images/favicon.png" />
-    <!-- Add the evo-calendar.css for styling -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/evo-calendar@1.1.2/evo-calendar/css/evo-calendar.min.css"/>
-
-
-
   </head>
   <body>
     <div class="container-scroller">
+
       <nav class="sidebar sidebar-offcanvas" id="sidebar">
-        <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
+
+      <!-- Adicionar Logo -->
+        <!-- <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
           <a class="sidebar-brand brand-logo" href="home.php"><img src="assets/images/logo.svg" alt="logo" /></a>
           <a class="sidebar-brand brand-logo-mini" href="home.php"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
-        </div>
+        </div> -->
+
+
         <ul class="nav">
           <li class="nav-item profile">
             <div class="profile-desc">
@@ -60,7 +67,7 @@
                   </div>
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item preview-item">
+                <a href="profile.php" class="dropdown-item preview-item">
                   <div class="preview-thumbnail">
                     <div class="preview-icon bg-dark rounded-circle">
                       <i class="mdi mdi-onepassword  text-info"></i>
@@ -98,7 +105,7 @@
 
 
           <li class="nav-item menu-items">
-            <a class="nav-link" href="blank-page.html">
+            <a class="nav-link" href="blank-page.php">
               <span class="menu-icon">
                 <i class="mdi mdi-contacts"></i>
               </span>
@@ -117,7 +124,7 @@
           </li>
 
 
-
+        <!-- apagar -->
           <li class="nav-item menu-items">
             <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
               <span class="menu-icon">
@@ -128,11 +135,11 @@
             </a>
             <div class="collapse" id="auth">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="blank-page.html"> Pagina em branco </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Pagina em branco </a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 500 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="index.php"> login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="register.php"> Registrar </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> login </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Registrar </a></li>
               </ul>
             </div>
           </li>
@@ -151,7 +158,10 @@
             </button>
             
             <ul class="navbar-nav navbar-nav-right">
-            
+              <li class="nav-item dropdown d-none d-lg-block">
+                <a class="nav-link btn btn-success create-new-button" id="createbuttonDropdown" href="devSoft.php">+ Criar novo projeto</a>
+                  </a>
+              </li>
 
 
               <li class="nav-item dropdown">
@@ -163,7 +173,7 @@
                   </div>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="profileDropdown">
-                  <h6 class="p-3 mb-0">Perfil</h6>
+                  <h6 class="p-3 mb-0">Profile</h6>
                   <div class="dropdown-divider"></div>
                   <a class="dropdown-item preview-item">
                     <div class="preview-thumbnail">
@@ -174,19 +184,19 @@
                     <div class="preview-item-content">
                       <p class="preview-subject mb-1">Configurações</p>
                     </div>
-                  </a>
-                  <div class = "logout" onclick="location.href='PHP/logout.php'"> 
-                    <a class="dropdown-item preview-item">
+                  </a> 
+                 <div class = "logout" onclick="location.href='PHP/logout.php'"> 
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item preview-item">
                     <div class="preview-thumbnail">
                         <div class="preview-icon bg-dark rounded-circle">
                             <i class="mdi mdi-logout text-danger"></i>
+                          </div>
                         </div>
-                      </div>
-                      <div class="preview-item-content">
-                        <p class="preview-subject mb-1">Log out</p>
-                      </div>
-                      </a>
-                  </div>
+                          <div class="preview-item-content">
+                          <p class="preview-subject mb-1">Log out</p>
+                        </div>
+                  </a>
               </li>
             </ul>
             <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
@@ -194,49 +204,118 @@
             </button>
           </div>
         </nav>
-
+      
         <div class="main-panel">
           <div class="content-wrapper">
             <div class="row">
-                <div class="col-md-12 grid-margin stretch-card">
-                  <div class="card">
-                    <div class="card-body">
-                    <div id="calendar">
-                    <script>
-                      $("#calendar").evoCalendar();
-                    </script>
+              <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="calendar">
+                        <h3 id="monthAndYear"></h3>
+                        <table class="table table-bordered table-responsive-sm" id="calendar">
+                        <thead>
+                        <tr>
+                            <th>Domingo</th>
+                            <th>Segunda</th>
+                            <th>Terça</th>
+                            <th>Quarta</th>
+                            <th>Quinta</th>
+                            <th>Sexta</th>
+                            <th>Sabado</th>
+                        </tr>
+                        </thead>
+                        <tbody id="calendar-body"></tbody>
+                        </table>
+                        
                     </div>
-                    
-  
-</div>
-</div>
-</div>
-</div>
-</div>
+                    <button id="previous" onclick="previous()">Previous</button>
+                    <button id="next" onclick="next()">Next</button>
+                    <label for="month">Jump To: </label>
+                        <select name="month" id="month" onchange="jump()">
+                            <option value=0>Jan</option>
+                            <option value=1>Feb</option>
+                            <option value=2>Mar</option>
+                            <option value=3>Apr</option>
+                            <option value=4>May</option>
+                            <option value=5>Jun</option>
+                            <option value=6>Jul</option>
+                            <option value=7>Aug</option>
+                            <option value=8>Sep</option>
+                            <option value=9>Oct</option>
+                            <option value=10>Nov</option>
+                            <option value=11>Dec</option>
+                        </select>
+                        <label for="year"></label>
+                        <select name="year" id="year" onchange="jump()">
+                            <option value=1990>1990</option>
+                            <option value=1991>1991</option>
+                            <option value=1992>1992</option>
+                            <option value=1993>1993</option>
+                            <option value=1994>1994</option>
+                            <option value=1995>1995</option>
+                            <option value=1996>1996</option>
+                            <option value=1997>1997</option>
+                            <option value=1998>1998</option>
+                            <option value=1999>1999</option>
+                            <option value=2000>2000</option>
+                            <option value=2001>2001</option>
+                            <option value=2002>2002</option>
+                            <option value=2003>2003</option>
+                            <option value=2004>2004</option>
+                            <option value=2005>2005</option>
+                            <option value=2006>2006</option>
+                            <option value=2007>2007</option>
+                            <option value=2008>2008</option>
+                            <option value=2009>2009</option>
+                            <option value=2010>2010</option>
+                            <option value=2011>2011</option>
+                            <option value=2012>2012</option>
+                            <option value=2013>2013</option>
+                            <option value=2014>2014</option>
+                            <option value=2015>2015</option>
+                            <option value=2016>2016</option>
+                            <option value=2017>2017</option>
+                            <option value=2018>2018</option>
+                            <option value=2019>2019</option>
+                            <option value=2020>2020</option>
+                            <option value=2021>2021</option>
+                            <option value=2022>2022</option>
+                            <option value=2023>2023</option>
+                            <option value=2024>2024</option>
+                            <option value=2025>2025</option>
+                            <option value=2026>2026</option>
+                            <option value=2027>2027</option>
+                            <option value=2028>2028</option>
+                            <option value=2029>2029</option>
+                            <option value=2030>2030</option>
+                        </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+         </div>
 
-<!-- <footer class="footer">
-<div class="d-sm-flex justify-content-center justify-content-sm-between">
-<span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright <?php print $nome; ?></span>
-</div>
-</footer> -->
-</div>
-</div>
+          <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright <?php print $nome; ?></span>
+              </div>
+          </footer>
 
-
-<!-- plugins:js -->
-
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/evo-calendar@1.1.2/evo-calendar/js/evo-calendar.min.js"></script>
-    <script src="assets/js/agenda.js"></script>
-    <script src="assets/js/calendar.js"></script>
+        </div>
+      </div>
+    </div>
+    
+    <!-- plugins:js -->
+    <script src="assets/js/calendario.js"></script>
     <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
     <script src="assets/js/off-canvas.js"></script>
     <script src="assets/js/hoverable-collapse.js"></script>
     <script src="assets/js/misc.js"></script>
     <script src="assets/js/settings.js"></script>
+    <script src="assets/js/todolist.js"></script>
     <script src="assets/js/dashboard.js"></script>
-    <script src="assets/js/evo-calendar.js"></script>
-
-
+ 
   </body>
 </html>
