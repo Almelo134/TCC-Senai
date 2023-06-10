dc886425708b948c4c602d17f0e042ca74c52505
Almelo134 <alissonhsmelo@gmail.com>
2023-06-10 05:10:49 -0300

    Update

--- PHP/fotoUpload.php
+++ PHP/fotoUpload.php
@@ -1,32 +1,35 @@
 <?php
-
 $id_usuario = $_SESSION['id_usuario'];
 
 // Lógica para identificar o perfil logado
 $perfilLogado = "$id_usuario";
 $imagemPadrao = 'assets/images/faces/face.jpg';
-$extensaoPadrao = pathinfo($imagemPadrao, PATHINFO_EXTENSION);      
-$path = 'assets/images/faces/'.$perfilLogado.'.jpg';
+$extensaoPadrao = pathinfo($imagemPadrao, PATHINFO_EXTENSION);
+$path = 'assets/images/faces/' . $perfilLogado . '.jpg';
 
 if (!file_exists($path)) {
   // Verifica se a imagem do perfil do usuário não existe
   if (copy($imagemPadrao, $path)) {
+    // Cópia da imagem padrão bem-sucedida
   }
 }
 
-if(isset($_POST['enviar'])){
+if (isset($_POST['enviar'])) {
   $arquivo = $_FILES['file'];
   $extensao = pathinfo($arquivo['name'], PATHINFO_EXTENSION);
-  $ex_permitidos = array('jpg', 'jpeg', 'png','svg');
+  $ex_permitidos = array('jpg', 'jpeg', 'png', 'svg');
 
-  if(!in_array(strtolower($extensao), $ex_permitidos)) {
+  if (!in_array(strtolower($extensao), $ex_permitidos)) {
     die('Você não pode fazer upload desse tipo de arquivo');
-  }
-  else {
-    // Movendo o arquivo para o diretório correto
-    move_uploaded_file($arquivo['tmp_name'], 'assets/images/faces/'.$perfilLogado.'.jpg');
-                  
-    // Exibir mensagem de sucesso
-    print '<script>alert("Upload efetuado com sucesso!!!")</script>';
-      }
+  } else {
+    $destino = 'assets/images/faces/' . $perfilLogado . '.jpg';
+    if (move_uploaded_file($arquivo['tmp_name'], $destino)) {
+      // Upload do arquivo bem-sucedido
+      echo '<script>alert("Upload efetuado com sucesso!!!")</script>';
+    } else {
+      // O upload do arquivo falhou
+      echo '<script>alert("Falha ao fazer upload do arquivo.")</script>';
     }
+  }
+}
+?>
--- profile.php
+++ profile.php
@@ -243,33 +243,39 @@ include 'PHP/updateProfile.php';
                       opacity: 1;
                   }
               </style>
+              <form action="" method="POST" enctype="multipart/form-data">
+                  <div class="card col-sm-12 grid-margin pt-4">
+                      <div class="image-wrapper">
+                          <?php
+                          $imagemPerfil = 'assets/images/faces/' . $perfilLogado . '.' . $extensaoPadrao;
 
+                          if (isset($_POST['enviar']) && isset($_FILES['file'])) {
+                              $arquivo = $_FILES['file'];
+                              $extensao = pathinfo($arquivo['name'], PATHINFO_EXTENSION);
 
-            <form action="" method="POST" enctype="multipart/form-data">
-                <div class="card col-sm-12 grid-margin pt-4">
-                    <div class="image-wrapper">
-                        <?php
-                        $imagemPerfil = 'assets/images/faces/'.$perfilLogado.'.'.$extensaoPadrao;
-                        if (isset($_POST['enviar']) && isset($_FILES['file'])) {
-                            $arquivo = $_FILES['file'];
-                            $extensao = pathinfo($arquivo['name'], PATHINFO_EXTENSION);
+                              if (in_array(strtolower($extensao), $ex_permitidos)) {
+                                  $destino = 'assets/images/faces/' . $perfilLogado . '.jpg';
 
-                            if (in_array(strtolower($extensao), $ex_permitidos)) {
-                                move_uploaded_file($arquivo['tmp_name'], 'assets/images/faces/'.$perfilLogado.'.jpg');
-                            }
-                        }
-                        if (file_exists($imagemPerfil)) {
-                            echo '<img class="img-xs-12" id="perfil-img" src="'.$imagemPerfil.'">';
-                        } else {
-                            echo '<img class="img-xs-12" id="perfil-img" src="'.$imagemPadrao.'">';
-                        }
-                        ?>
-                        <label for="file" class="overlay">Trocar foto de perfil</label>
-                        <input type="file" id="file" name="file" style="display: none;" onchange="trocarImagem(event)"> 
+                                  if (move_uploaded_file($arquivo['tmp_name'], $destino)) {
+                                      // Upload do arquivo bem-sucedido
+                                      echo '<script>alert("Upload efetuado com sucesso!!!")</script>';
+                                      $imagemPerfil = $destino; // Atualiza o caminho da imagem exibida
+                                  }
+                              }
+                          }
+
+                          if (file_exists($imagemPerfil)) {
+                              echo '<img class="img-xs-12" id="perfil-img" src="' . $imagemPerfil . '">';
+                          } else {
+                              echo '<img class="img-xs-12" id="perfil-img" src="' . $imagemPadrao . '">';
+                          }
+                          ?>
+                          <label for="file" class="overlay">Trocar foto de perfil</label>
+                          <input type="file" id="file" name="file" style="display: none;" onchange="trocarImagem(event)">
                       </div>
-                      <input type="submit" name="enviar" value="Enviar" id="enviar-btn" style="display: none;" class="btn btn-primary"> 
-                </div>
-            </form>
+                      <input type="submit" name="enviar" value="Enviar" id="enviar-btn" style="display: none;" class="btn btn-primary">
+                  </div>
+              </form>
 
             <script>
               function trocarImagem(event) {        
